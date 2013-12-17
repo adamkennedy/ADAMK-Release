d1eac69ba5c3006363f00588a02b0b2c03915c65
adamkennedy <adam.kennedy@kaggle.com>
2013-12-16 22:32:04 -0800

    Added Test::Script

--- lib/ADAMK/Release.pm
+++ lib/ADAMK/Release.pm
@@ -775,3 +775,33 @@ sub error {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+ADAMK::Release - 
+
+=head1 DESCRIPTION
+
+C<ADAMK::Release> is the backend behind the C<adamk-release> script that
+is used to build distribution tarballs for modules with the minimalist
+repository style.
+
+=head1 AUTHORS
+
+Adam Kennedy E<lt>adamk@cpan.orgE<gt>
+
+=head1 SEE ALSO
+
+L<http://ali.as/>
+
+=head1 COPYRIGHT
+
+Copyright 2013 Adam Kennedy.
+
+This program is free software; you can redistribute
+it and/or modify it under the same terms as Perl itself.
+
+The full text of the license can be found in the
+LICENSE file included with this module.
--- t/01_compile.t
+++ t/01_compile.t
@@ -3,6 +3,8 @@
 use 5.10.0;
 use strict;
 use warnings;
-use Test::More tests => 1;
+use Test::More tests => 2;
+use Test::Script;
 
 use_ok('ADAMK::Release');
+script_compiles('script/adamk-release');
